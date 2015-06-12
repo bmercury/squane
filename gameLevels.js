546c7e1248ee90730afa7c57367a2a1ce80ba687
LaughingApe <niksderums@inbox.lv>
2015-06-12 16:43:46 +0300

    iesākta ģenerēšana

--- gameLevels.js
+++ gameLevels.js
@@ -1,7 +1,7 @@
 var level = 1;
 var score = 0;
 
-var colorCount = [
+var levelData = [
 	{rows:2,colors:2,speed:0}, // kolonas,krasas,atrums
 	{rows:2,colors:3,speed:0},
 	{rows:2,colors:4,speed:0}
@@ -16,11 +16,15 @@ var colorMap = [
 ];
 
 function mainLevelFunction(thisLevel){
-		
+	createSquares(thisLevel);
 }
 
 function createSquares(thisLevel){
-	
+	var cellCount = levelData[thisLevel][rows]*levelData[thisLevel][rows];
+	for(var i = 0; i<cellCount; i++){
+		var thisColor = Math.floor((Math.random() * level));
+		document.getElementById("#gameTable").innerHTML = "<div class='color" + thisColor + "' id='block" + i + "'></div>";
+	}
 }
 
 function hideSquares(thisLevel){
