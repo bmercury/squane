1c11dc095bb78d23ff47602f0b51f5e19c9629f8
LaughingApe <niksderums@inbox.lv>
2015-06-12 16:12:40 +0300

    Speles css  un js

---
+++ gameLevels.js
@@ -0,0 +1,15 @@
+var level = 1;
+
+var colorCount = [
+	{rows:2,colors:2,speed:0}, // kolonas,krasas,atrums
+	{rows:2,colors:3,speed:0},
+	{rows:2,colors:4,speed:0}
+];
+
+var colorMap = [
+	"square-blue.png",
+	"square-red.png",
+	"square-green.png",
+	"square-yellow.png",
+	"square-purple.png"
+];
