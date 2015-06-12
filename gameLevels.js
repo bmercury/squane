8cc7d3319cfc6affb48089c28a967ae3e21ce32c
LaughingApe <niksderums@inbox.lv>
2015-06-12 17:16:15 +0300

    strādājoša ģenerācija

--- gameLevels.js
+++ gameLevels.js
@@ -1,24 +1,33 @@
-var level = 1;
+var level = 0;
 var score = 0;
 
 var levelData = [
-	{rows:2,colors:2,speed:0}, // kolonas,krasas,atrums
+	{rows:2,colors:5,speed:0}, // kolonas,krasas,atrums
 	{rows:2,colors:3,speed:0},
 	{rows:2,colors:4,speed:0}
 ];
+
 
 function mainLevelFunction(thisLevel){
 	createSquares(thisLevel);
 }
 
 function createSquares(thisLevel){
-	var cellCount = levelData[thisLevel][rows]*levelData[thisLevel][rows];
+	var cellCount = levelData[thisLevel].rows*levelData[thisLevel].rows;
+
+	var squaneTable = "";
+
 	for(var i = 0; i<cellCount; i++){
-		var thisColor = Math.floor((Math.random() * level));
-		document.getElementById("#gameTable").innerHTML = "<div class='color" + thisColor + "' id='block" + i + "'></div>";
+		var thisColor = Math.floor(Math.random() * (levelData[thisLevel].colors));
+		squaneTable = squaneTable + "<div class='block size" + levelData[thisLevel].rows + " color" + thisColor + "' id='block" + i + "'></div>";
+		//alert(squaneTable);
 	}
+	document.getElementById("gameTable").innerHTML = squaneTable;
 }
 
 function hideSquares(thisLevel){
 
+}
+window.onload = function() {
+	mainLevelFunction(level);
 }
