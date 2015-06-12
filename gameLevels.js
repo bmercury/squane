d469d4f876c300d1a0bf08714f8b517c03014074
Reinards <hiperpile@gmail.com>
2015-06-12 21:20:50 +0300

    kluchi

--- gameLevels.js
+++ gameLevels.js
@@ -13,9 +13,17 @@ var speedData = [
 	1000
 ];
 
+var clicks = [];
 
 function mainLevelFunction(thisLevel){
 	createSquares(thisLevel);
+	resetSquares(levelData[level].rows*levelData[level].rows);
+}
+
+function resetSquares(n){
+	for(var i=0;i<n;i++){
+		clicks[i]=0;
+	}
 }
 
 var ans = [];
@@ -51,7 +59,30 @@ function startInvestigation(){
 	document.getElementById("currentPlace").innerHTML = "<div class='choise color" + gThisColor + "'></div>";
 }
 
+function choiseDone(i){
+	var choise=null;
+	if($("#block"+i).hasClass( "color0" ))choise=0;
+	else if($("#block"+i).hasClass( "color1" ))choise=1;
+	else if($("#block"+i).hasClass( "color2" ))choise=2;
+	else if($("#block"+i).hasClass( "color3" ))choise=3;
+	else if($("#block"+i).hasClass( "color4" ))choise=4;
 
+	if(clicks[i]!=1){
+		clicks[i]=1;
+		if(choise == gThisColor){
+		// Izvēlēts pareizais kvadrārs
+		score+=1;
+		document.getElementById("score").innerHTML="Punkti: "+score;
+		$("#block"+i).removeClass("no-color");
+	}else{
+		// Izvēlēts nepareizais kvadrārs
+		score-=2;
+		document.getElementById("score").innerHTML="Punkti: "+score;
+		$("#block"+i).removeClass("no-color");
+	}
+	}
+
+}
 
 window.onload = function() {
 	mainLevelFunction(level);
