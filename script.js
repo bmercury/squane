d81f2e8c5039e12a1a10d7ca235469fa1ccf3fcd
Reinards <hiperpile@gmail.com>
2015-06-30 10:56:44 +0300

    mēģinu izlabot bg erroru

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function setBg(){
     var color = "";
     
     if(localStorage.getItem("bg") === null){
-        localStorage.setItem("bg",0);
+        localStorage.setItem("bg",1);
     }
     if(localStorage.getItem("bg") == 0){
         bg=bgColorLight;
@@ -30,6 +30,7 @@ function setBg(){
     }
     $("body").css('background-color', bg);
     $("body").css('color', color);
+
 }
 
 window.onload = function() {
