e20d8b22b6ee46e24fdea0a4cc5b3d968f171c26
LaughingApe <niksderums@inbox.lv>
2015-09-25 16:36:49 +0300

    Iesākta veikala dizaina maiņa

--- scriptShop.js
+++ scriptShop.js
@@ -44,7 +44,9 @@ function chooseDes(i){
         }   
 
     }
-    location.reload();
+    //location.reload();
+    document.getElementById("previewBox").innerHTML = "";
+    showDesigns();
 }
 
 function showDesigns(){
@@ -62,10 +64,13 @@ function showDesigns(){
         var underText = "<b style=\"color: darkgreen;\">Bought</b>";
         var haventBoughtClass = "";
         if(i>=boughtDesigns){
-            underText = "<img width=\"15px\" style=\"display:inline;vertical-align: middle;\" src=\"images/sqoin.png\">" + cost[i]; 
+            underText = "<img width=\"15px\" style=\"display:inline;vertical-align: middle;\" src=\"images/sqoin.png\"> " + cost[i]; 
             haventBoughtClass = " hvntBought";
         }
-        if(i==boughtDesigns) haventBoughtClass = " canBuy";
+        if(i==boughtDesigns) {
+            //underText = "<img width=\"15px\" style=\"display:inline;vertical-align: middle;\" src=\"images/sqoin.png\"> " + cost[i]; 
+            haventBoughtClass = " canBuy";
+        }
 
         var anotherDesign = "<div onclick=\"chooseDes(" + i + ");\" class=\"designPreviewBox" + chosedDes + haventBoughtClass + "\"> <table><tr><td><img class=\"previewSquane\" src=\" images/pack" + i + "/square-blue.png \"></img> </td><td><img class=\"previewSquane\" src=\" images/pack" + i + "/square-red.png \"></img> </td></tr><tr><td><img class=\"previewSquane\" src=\" images/pack" + i + "/square-green.png \"></img> </td><td><img class=\"previewSquane\" src=\" images/pack" + i + "/square-yellow.png \"></img> </td></tr></table> <span>" + underText + "</span> </div>";        catalog = catalog + anotherDesign;
     }
